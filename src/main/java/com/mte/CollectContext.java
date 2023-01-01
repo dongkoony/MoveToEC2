fef4f9b642fd1ff78ab8681d8290a6a867e866f9
dongkoony <donghyun4591@gmail.com>
2023-01-02 00:23:04 +0900

    Context,Dto 추가

---
+++ src/main/java/com/mte/CollectContext.java
@@ -0,0 +1,40 @@
+package com.mte;
+
+public class CollectContext {
+    private String ipaddress;
+    private String user;
+    private String port;
+    private String password;
+
+    public String getIpaddress() {
+        return ipaddress;
+    }
+
+    public void setIpaddress(String ipaddress) {
+        this.ipaddress = ipaddress;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- src/main/java/com/mte/config/MTEDataSourceConfig.java
+++ src/main/java/com/mte/config/MTEDataSourceConfig.java
@@ -1,4 +1,4 @@
-package com.mte.com.mte.config;
+package com.mte.config;
 
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
